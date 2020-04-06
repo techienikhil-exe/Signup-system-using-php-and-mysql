252f786d4ca46d9dfd3e09fb635fd72a734137d4
Nikhil Babu Manchukonda <38748498+techienikhil-exe@users.noreply.github.com>
2020-04-06 12:14:06 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,44 +1,42 @@
 <?php 
-	session_start();
+session_start();
 
-	// variable declaration
-	$username = "";
-	$email    = "";
-	$errors = array(); 
-	$_SESSION['success'] = "";
+//variable declaration
+$username = "";
+$email    = "";
+$errors = array(); 
+$_SESSION['success'] = "";
 
-	// connect to database
-	$db = mysqli_connect('localhost', 'root', '', 'sign up');
+// connect to database
+$db = mysqli_connect('localhost', 'root', '', 'sign up');
 
-	// REGISTER USER
-	if (isset($_POST['reg_user'])) {
-		// receive all input values from the form
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+//signing up USER
+if (isset($_POST['reg_user'])) {
 
-		// form validation: ensure that the form is correctly filled
-		if (empty($username)) { array_push($errors, "Username is required"); }
-		if (empty($email)) { array_push($errors, "Email is required"); }
-		if (empty($password_1)) { array_push($errors, "Password is required"); }
+//receive all input values from the signup form
+$username = mysqli_real_escape_string($db, $_POST['username']);
+$email = mysqli_real_escape_string($db, $_POST['email']);
+$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-		if ($password_1 != $password_2) { array_push($errors, "The two passwords do not match");}
-		if (! isset($_POST["terms"])) { array_push($errors, "Please accept the Terms of Use & Privacy Policy");
-										$valid = false;
-            }
+//signup form validation
+if (empty($username)) { array_push($errors, "Username is required"); }
+if (empty($email)) { array_push($errors, "Email is required"); }
+if (empty($password_1)) { array_push($errors, "Password is required"); }
+if ($password_1 != $password_2) { array_push($errors, "The two passwords do not match");}
+if (! isset($_POST["terms"])) { array_push($errors, "Please accept the Terms of Use & Privacy Policy");
+$valid = false;
+}
 
-		// register user if there are no errors in the form
-		if (count($errors) == 0) {
-			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO users (username, email, password) 
-					  VALUES('$username', '$email', '$password')";
-			mysqli_query($db, $query);
-
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "Your details have been validated";
-			header('location: validate.php');
-		}
-
-	}
+// signing up user if there are no errors in the form
+if (count($errors) == 0) {
+$password = md5($password_1);//encrypt the password before saving in the database
+$query = "INSERT INTO users (username, email, password) 
+VALUES('$username', '$email', '$password')";
+mysqli_query($db, $query);
+$_SESSION['username'] = $username;
+$_SESSION['success'] = "Your details have been validated";
+header('location: validate.php');
+}
+}
 ?>
